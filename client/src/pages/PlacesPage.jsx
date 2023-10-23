a418ec61ad7c7990b2ec457cf339ffe406f9d828
Parinya Namkaew <645021000742@mail.rmutk.ac.th>
2023-10-23 17:33:57 +0700

    Night Commit

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import AccountNav from "../AccountNav";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import HotelsPicture from "../HotelsPicture";
+
 
 export default function PlacesPage() {
     const [places, setPlaces] = useState([]);
@@ -29,7 +29,7 @@ export default function PlacesPage() {
                 {places.length > 0 && places.map(place => (
                     <Link to={'/account/places/' + place._id} key={place._id} className=" flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
                         <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
-                          <HotelsPicture place={place}/>
+                          
                         </div>
                         <div className="grow-0 shrink">
                             <h2 className="text-xl font-bold ">{place.title}</h2>
